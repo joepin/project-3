91a6cbde7bbc01f4e7c0a1a5c7a7912de4f2fc47
Joey Pinhas <jpinhas97@yahoo.com>
2016-11-25 09:49:25 -0500

    more file upload testing

--- server.js
+++ server.js
@@ -8,16 +8,11 @@ const bodyParser   = require('body-parser');
 const app          = express();
 const PORT         = process.argv[2] || process.env.port || 3000;
 
-const multer = require('multer');
-
 app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(multer().none())
 
 app.use('/api/v1', require('./routes/api.js'));
 
